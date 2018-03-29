085391d40ddf7e8bb12aa53d81714ca136679c50
KatiaVet <36220557+KatiaVet@users.noreply.github.com>
2018-03-29 22:58:30 +0300

    Add files via upload

--- rabota.R
+++ rabota.R
@@ -54,27 +54,18 @@ teach = sample(row_numbers, floor(length(tbl$date)*.7))
 test = row_numbers[-teach] 
 teaching_tbl_unq = tbl[teach,] 
 testing_tbl_unq = tbl[test,]
-mod1 = lm(h2o_flux~(DOY+Tau+rand_err_Tau + H+rand_err_H +LE+ rand_err_LE +co2_flux+rand_err_co2_flux+ rand_err_h2o_flux + H_strg + co2_molar_density +h2o_time_lag + sonic_temperature + 
-                      + air_temperature+air_density+air_molar_volume+es+RH+VPD+u_star_+TKE+T_star_+un_H+un_Tau+un_LE+un_co2_flux+un_h2o_flux+flowrate)^2,data=tbl)
+mod1 = lm(h2o_flux~(LE + rand_err_LE + rand_err_h2o_flux + 
+                      un_LE + un_h2o_flux + w_div_h2o_cov)^2,data=tbl)
 summary(mod1) 
 predict(mod1) 
 resid(mod1) 
 coef(mod1) 
 anova(mod1) 
 plot(mod1)
-mod2 = lm(h2o_flux~(DOY+Tau+rand_err_Tau + H+rand_err_H +LE+ rand_err_LE +co2_flux+rand_err_co2_flux+ rand_err_h2o_flux + H_strg + co2_molar_density +h2o_time_lag + sonic_temperature + 
-                      + air_temperature+air_density+air_molar_volume+es+RH+VPD+u_star_+TKE+T_star_+un_H+un_Tau+un_LE+un_co2_flux+un_h2o_flux+flowrate)^2-DOY:Tau-DOY:air_density-DOY:un_h2o_flux-Tau:RH-rand_err_Tau:LE,data=tbl)
+mod2 = lm(h2o_flux~(LE + rand_err_h2o_flux)^2,data=tbl)
 summary(mod2) 
 predict(mod2) 
 resid(mod2) 
 coef(mod2) 
 anova(mod2) 
 plot(mod2)
-mod3 = lm(h2o_flux~(DOY+Tau+rand_err_Tau + H+rand_err_H +LE+ rand_err_LE +co2_flux+rand_err_co2_flux+ rand_err_h2o_flux + H_strg + co2_molar_density +h2o_time_lag + sonic_temperature + 
-                      + air_temperature+air_density+air_molar_volume+es+RH+VPD+u_star_+TKE+T_star_+un_H+un_Tau+un_LE+un_co2_flux+un_h2o_flux+flowrate)^2-u_unrot-v_unrot-w_unrot-yaw-pitch,data=tbl)
-summary(mod3) 
-predict(mod3) 
-resid(mod3) 
-coef(mod3) 
-anova(mod3) 
-plot(mod3)
